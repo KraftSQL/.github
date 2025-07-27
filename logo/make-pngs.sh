a3fd6d999e5978bd21bcbc4f9f516d9eb1b2d0c1
Frieler, Christopher <christopher@frieler.rocks>
2025-07-27 22:02:35 +0200

    Design a logo

---
+++ logo/make-pngs.sh
@@ -0,0 +1,2 @@
+sips -s format png logo.line.svg -o logo.line.png --resampleWidth 720 --resampleHeight 180
+sips -s format png logo.square.svg -o logo.square.png --resampleWidth 400 --resampleHeight 400
